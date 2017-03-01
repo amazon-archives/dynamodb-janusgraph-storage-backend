03e465f58b8c4176b61fb8660c02fe89f666a228
Alexander Patrikalakis <amcp@amazon.co.jp>
2017-03-02 00:36:50 +0900

    remove copied titan code and tests; fix testCompile target

--- src/test/java/com/amazon/janusgraph/graphdb/dynamodb/AbstractDynamoDBGraphConcurrentTest.java
+++ src/test/java/com/amazon/janusgraph/graphdb/dynamodb/AbstractDynamoDBGraphConcurrentTest.java
@@ -1,7 +1,5 @@
 /*
  * Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * Portions copyright Titan: Distributed Graph Database - Copyright 2012 and onwards Aurelius.
-
  *
  * Licensed under the Apache License, Version 2.0 (the "License").
  * You may not use this file except in compliance with the License.
@@ -16,17 +14,7 @@
  */
 package com.amazon.janusgraph.graphdb.dynamodb;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-
-import com.amazon.janusgraph.testutils.CiHeartbeat;
-import com.amazon.janusgraph.TestGraphUtil;
-import com.amazon.janusgraph.diskstorage.dynamodb.BackendDataModel;
-import com.google.common.collect.Iterables;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -35,7 +23,9 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestName;
 
-import org.janusgraph.core.JanusGraphVertex;
+import com.amazon.janusgraph.testutils.CiHeartbeat;
+import com.amazon.janusgraph.TestGraphUtil;
+import com.amazon.janusgraph.diskstorage.dynamodb.BackendDataModel;
 import org.janusgraph.diskstorage.BackendException;
 import org.janusgraph.diskstorage.configuration.WriteConfiguration;
 import org.janusgraph.graphdb.JanusGraphConcurrentTest;
@@ -85,55 +75,7 @@ public abstract class AbstractDynamoDBGraphConcurrentTest extends JanusGraphConc
     @Test
     @Override
     public void testStandardIndexVertexPropertyReads() throws InterruptedException, ExecutionException {
-        final int propCount = 20; //THREAD_COUNT * 5;
-        final int vertexCount = 1 * 1000;
-        // Create props with standard indexes
-        for (int i = 0; i < propCount; i++) {
-            makeVertexIndexedUniqueKey("p"+i,String.class);
-        }
-        finishSchema();
-
-        // Write vertices with indexed properties
-        for (int i = 0; i < vertexCount; i++) {
-            JanusGraphVertex v = tx.addVertex();
-            for (int p = 0; p < propCount; p++) {
-                v.property("p" + p, i);
-            }
-        }
-        newTx();
-        // Execute runnables
-        final int taskCount = 4 * 256;
-        final ExecutorService executor = Executors.newFixedThreadPool(256);
-        Collection<Future<?>> futures = new ArrayList<Future<?>>(taskCount);
-        for (int i = 0; i < taskCount; i++) {
-            futures.add(executor.submit(new VertexPropertyQuerier(propCount, vertexCount)));
-        }
-        for (Future<?> f : futures) {
-            f.get();
-        }
+        //this test takes too long so reduce the number of threads
+        testStandardIndexVertexPropertyReadsLogic(4 /*numThreads*/);
     }
-    //https://github.com/thinkaurelius/titan/blob/0.9.0-M2/titan-test/src/main/java/com/thinkaurelius/titan/graphdb/TitanGraphConcurrentTest.java#L320
-
-    //begin janusgraph-test code - this was private so needed to copy
-    //https://github.com/thinkaurelius/titan/blob/0.9.0-M2/titan-test/src/main/java/com/thinkaurelius/titan/graphdb/TitanGraphConcurrentTest.java#L456
-    private class VertexPropertyQuerier implements Runnable {
-
-        private final int propCount;
-        private final int vertexCount;
-
-        public VertexPropertyQuerier(int propCount, int vertexCount) {
-            this.propCount = propCount;
-            this.vertexCount = vertexCount;
-        }
-
-        @Override
-        public void run() {
-            for (int i = 0; i < vertexCount; i++) {
-                for (int p = 0; p < propCount; p++) {
-                    Iterables.size(tx.query().has("p" + p, i).vertices());
-                }
-            }
-        }
-    }
-    //https://github.com/thinkaurelius/titan/blob/0.9.0-M2/titan-test/src/main/java/com/thinkaurelius/titan/graphdb/TitanGraphConcurrentTest.java#L474
 }
