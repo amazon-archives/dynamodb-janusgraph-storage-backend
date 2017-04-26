10d73ebf49b9de4b42d708eeb155e77496219dbd
Alexander Patrikalakis <amcp@amazon.co.jp>
2017-04-26 17:31:24 +0800

    Isolated long-running *Store and *Graph tests

--- src/test/java/com/amazon/titan/diskstorage/dynamodb/SingleDynamoDBStoreTest.java
+++ src/test/java/com/amazon/titan/diskstorage/dynamodb/SingleDynamoDBStoreTest.java
@@ -14,19 +14,215 @@
  */
 package com.amazon.titan.diskstorage.dynamodb;
 
-import com.amazon.titan.testcategory.SingleDynamoDBStoreTestCategory;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+import org.junit.Test;
 import org.junit.experimental.categories.Category;
+
+import com.amazon.titan.testcategory.SingleDynamoDBStoreTestCategory;
+import com.thinkaurelius.titan.diskstorage.BackendException;
 
 /**
  *
  * @author Alexander Patrikalakis
  *
  */
-@Category({ SingleDynamoDBStoreTestCategory.class })
 public class SingleDynamoDBStoreTest extends AbstractDynamoDBStoreTest
 {
     public SingleDynamoDBStoreTest()
     {
         super(BackendDataModel.SINGLE);
+    }
+
+    @Test
+    @Override
+    @Category({SingleDynamoDBStoreTestCategory.class})
+    public void parallelScanTest() throws Exception {
+        super.parallelScanTest();
+    }
+
+    @Test
+    @Override
+    @Category({SingleDynamoDBStoreTestCategory.class})
+    public void testStoreTTL() throws Exception {
+        super.testStoreTTL();
+    }
+
+    @Test
+    @Override
+    @Category({SingleDynamoDBStoreTestCategory.class})
+    public void storeAndRetrieveWithClosing() throws BackendException {
+        super.storeAndRetrieveWithClosing();
+    }
+
+    @Test
+    @Override
+    @Category({SingleDynamoDBStoreTestCategory.class})
+    public void containsKeyColumnReturnsTrueOnExtantInput() throws Exception {
+        super.containsKeyColumnReturnsTrueOnExtantInput();
+    }
+
+    @Test
+    @Override
+    @Category({SingleDynamoDBStoreTestCategory.class})
+    public void containsKeyColumnReturnsFalseOnNonexistentInput() throws Exception {
+        super.containsKeyColumnReturnsFalseOnNonexistentInput();
+    }
+
+    @Test
+    @Override
+    @Category({SingleDynamoDBStoreTestCategory.class})
+    public void createDatabase() {
+        super.createDatabase();
+    }
+
+    @Test
+    @Override
+    @Category({SingleDynamoDBStoreTestCategory.class})
+    public void intervalTest1() throws BackendException {
+        super.intervalTest1();
+    }
+
+    @Test
+    @Override
+    @Category({SingleDynamoDBStoreTestCategory.class})
+    public void intervalTest2() throws BackendException {
+        super.intervalTest2();
+    }
+
+    @Test
+    @Override
+    @Category({SingleDynamoDBStoreTestCategory.class})
+    public void scanTestWithSimpleJob() throws Exception {
+        super.scanTestWithSimpleJob();
+    }
+
+    @Test
+    @Override
+    @Category({SingleDynamoDBStoreTestCategory.class})
+    public void testGetKeysColumnSlicesOnLowerTriangular() throws BackendException, IOException {
+        super.testGetKeysColumnSlicesOnLowerTriangular();
+    }
+
+    @Test
+    @Override
+    @Category({SingleDynamoDBStoreTestCategory.class})
+    public void testGetKeysColumnSlicesSimple() throws BackendException {
+        super.testGetKeysColumnSlicesSimple();
+    }
+
+    @Test
+    @Override
+    @Category({SingleDynamoDBStoreTestCategory.class})
+    public void getSliceRespectsAllBoundsInclusionArguments() throws Exception {
+        super.getSliceRespectsAllBoundsInclusionArguments();
+    }
+
+    @Test
+    @Override
+    @Category({SingleDynamoDBStoreTestCategory.class})
+    public void scanTest() throws BackendException {
+        super.scanTest();
+    }
+
+    @Test
+    @Override
+    @Category({SingleDynamoDBStoreTestCategory.class})
+    public void deleteColumnsTest1() throws BackendException {
+        super.deleteColumnsTest1();
+    }
+
+    @Test
+    @Override
+    @Category({SingleDynamoDBStoreTestCategory.class})
+    public void deleteColumnsTest2() throws BackendException {
+        super.deleteColumnsTest2();
+    }
+
+    @Test
+    @Override
+    @Category({SingleDynamoDBStoreTestCategory.class})
+    public void getNonExistentKeyReturnsNull() throws Exception {
+        super.getNonExistentKeyReturnsNull();
+    }
+
+    @Test
+    @Override
+    @Category({SingleDynamoDBStoreTestCategory.class})
+    public void storeAndRetrievePerformance() throws BackendException {
+        super.storeAndRetrievePerformance();
+    }
+
+    @Test
+    @Override
+    @Category({SingleDynamoDBStoreTestCategory.class})
+    public void storeAndRetrieve() throws BackendException {
+        super.storeAndRetrieve();
+    }
+
+    @Test
+    @Override
+    @Category({SingleDynamoDBStoreTestCategory.class})
+    public void containsKeyReturnsTrueOnExtantKey() throws Exception {
+        super.containsKeyReturnsTrueOnExtantKey();
+    }
+
+    @Test
+    @Override
+    @Category({SingleDynamoDBStoreTestCategory.class})
+    public void getSliceRespectsColumnLimit() throws Exception {
+        super.getSliceRespectsColumnLimit();
+    }
+
+    @Test
+    @Override
+    @Category({SingleDynamoDBStoreTestCategory.class})
+    public void containsKeyReturnsFalseOnNonexistentKey() throws Exception {
+        super.containsKeyReturnsFalseOnNonexistentKey();
+    }
+
+    @Test
+    @Override
+    @Category({SingleDynamoDBStoreTestCategory.class})
+    public void testGetKeysWithSliceQuery() throws Exception {
+        super.testGetKeysWithSliceQuery();
+    }
+
+    @Test
+    @Override
+    @Category({SingleDynamoDBStoreTestCategory.class})
+    public void insertingGettingAndDeletingSimpleDataWorks() throws Exception {
+        super.insertingGettingAndDeletingSimpleDataWorks();
+    }
+
+    public interface IsolateTestConcurrentGetSliceAndMutate { }
+    @Test
+    @Override
+    @Category({IsolateTestConcurrentGetSliceAndMutate.class})
+    public void testConcurrentGetSliceAndMutate() throws ExecutionException, InterruptedException, BackendException {
+        super.testConcurrentGetSliceAndMutate();
+    }
+
+    @Test
+    @Override
+    @Category({SingleDynamoDBStoreTestCategory.class})
+    public void testGetSlices() throws Exception {
+        super.testGetSlices();
+    }
+
+    @Test
+    @Override
+    @Category({SingleDynamoDBStoreTestCategory.class})
+    public void deleteKeys() throws BackendException {
+        super.deleteKeys();
+    }
+
+    public interface IsolateTestConcurrentGetSlice { }
+    @Test
+    @Override
+    @Category({IsolateTestConcurrentGetSlice.class})
+    public void testConcurrentGetSlice() throws ExecutionException, InterruptedException, BackendException {
+        super.testConcurrentGetSlice();
     }
 }
--- src/test/java/com/amazon/titan/graphdb/dynamodb/MultiDynamoDBGraphTest.java
+++ src/test/java/com/amazon/titan/graphdb/dynamodb/MultiDynamoDBGraphTest.java
@@ -14,21 +14,448 @@
  */
 package com.amazon.titan.graphdb.dynamodb;
 
+import java.util.concurrent.ExecutionException;
+
+import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 import com.amazon.titan.diskstorage.dynamodb.BackendDataModel;
-import com.amazon.titan.testcategory.MultipleItemTestCategory;
+import com.amazon.titan.testcategory.MultiDynamoDBGraphTestCategory;
+import com.thinkaurelius.titan.diskstorage.BackendException;
 
 /**
- *
  * @author Alexander Patrikalakis
- *
  */
-@Category({ MultipleItemTestCategory.class })
-public class MultiDynamoDBGraphTest extends AbstractDynamoDBGraphTest
-{
-    public MultiDynamoDBGraphTest()
-    {
+public class MultiDynamoDBGraphTest extends AbstractDynamoDBGraphTest {
+    public MultiDynamoDBGraphTest() {
         super(BackendDataModel.MULTI);
+    }
+
+    //TODO in super superclass TitanGraphTest make sure exception signature resolves to something
+    //more specific than Exception.class
+
+    //$ cat TEST-com.amazon.titan.graphdb.dynamodb.MultiDynamoDBGraphTest.xml | grep testcase | sed \
+    // 's/.*\ name=\"\([^\"]*\)\".*/\ \ \ \ @Category({\ MultiDynamoDBGraphTestCategory.class\ })\ public\ void\ \1()\ {\ super\.\1();\ }/'
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testOpenClose() {
+        super.testOpenClose();
+    }
+
+    public interface IsolateTestLargeJointIndexRetreival {
+    }
+
+    @Test
+    @Override
+    @Category({IsolateTestLargeJointIndexRetreival.class})
+    public void testLargeJointIndexRetrieval() {
+        super.testLargeJointIndexRetrieval();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testMediumCreateRetrieve() {
+        super.testMediumCreateRetrieve();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testSettingTTLOnUnsupportedType() throws Exception {
+        super.testSettingTTLOnUnsupportedType();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testSchemaNameChange() {
+        super.testSchemaNameChange();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void simpleLogTest() throws InterruptedException {
+        super.simpleLogTest();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testSchemaTypes() {
+        super.testSchemaTypes();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testTinkerPopOptimizationStrategies() {
+        super.testTinkerPopOptimizationStrategies();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testGetTTLFromUnsupportedType() throws Exception {
+        super.testGetTTLFromUnsupportedType();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testLocalGraphConfiguration() {
+        super.testLocalGraphConfiguration();
+    }
+
+    public interface IsolateTestVertexCentricQuery {
+    }
+
+    @Test
+    @Override
+    @Category({IsolateTestVertexCentricQuery.class})
+    public void testVertexCentricQuery() {
+        super.testVertexCentricQuery();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testConcurrentConsistencyEnforcement() throws Exception {
+        super.testConcurrentConsistencyEnforcement();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testTransactionalScopeOfSchemaTypes() {
+        super.testTransactionalScopeOfSchemaTypes();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testNestedTransactions() {
+        super.testNestedTransactions();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testBasic() {
+        super.testBasic();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testUnsettingTTL() throws InterruptedException {
+        super.testUnsettingTTL();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testGlobalOfflineGraphConfig() {
+        super.testGlobalOfflineGraphConfig();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testLimitWithMixedIndexCoverage() {
+        super.testLimitWithMixedIndexCoverage();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testMultivaluedVertexProperty() {
+        super.testMultivaluedVertexProperty();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testGlobalGraphConfig() {
+        super.testGlobalGraphConfig();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testManagedOptionMasking() throws BackendException {
+        super.testManagedOptionMasking();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testGlobalGraphIndexingAndQueriesForInternalIndexes() {
+        super.testGlobalGraphIndexingAndQueriesForInternalIndexes();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testWithoutIndex() {
+        super.testWithoutIndex();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testIndexUpdatesWithReindexAndRemove() throws InterruptedException, ExecutionException {
+        super.testIndexUpdatesWithReindexAndRemove();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testEdgeTTLTiming() throws Exception {
+        super.testEdgeTTLTiming();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testStaleVertex() {
+        super.testStaleVertex();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testGettingUndefinedVertexLabelTTL() {
+        super.testGettingUndefinedVertexLabelTTL();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void simpleLogTestWithFailure() throws InterruptedException {
+        super.simpleLogTestWithFailure();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testVertexCentricIndexWithNull() {
+        super.testVertexCentricIndexWithNull();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testVertexTTLImplicitKey() throws Exception {
+        super.testVertexTTLImplicitKey();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testImplicitKey() {
+        super.testImplicitKey();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testMaskableGraphConfig() {
+        super.testMaskableGraphConfig();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testDataTypes() throws Exception {
+        super.testDataTypes();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testEdgeTTLImplicitKey() throws Exception {
+        super.testEdgeTTLImplicitKey();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testTinkerPopCardinality() {
+        super.testTinkerPopCardinality();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testPropertyCardinality() {
+        super.testPropertyCardinality();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testArrayEqualityUsingImplicitKey() {
+        super.testArrayEqualityUsingImplicitKey();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testFixedGraphConfig() {
+        super.testFixedGraphConfig();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testAutomaticTypeCreation() {
+        super.testAutomaticTypeCreation();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testGettingUndefinedEdgeLabelTTL() {
+        super.testGettingUndefinedEdgeLabelTTL();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testSimpleTinkerPopTraversal() {
+        super.testSimpleTinkerPopTraversal();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testGlobalIteration() {
+        super.testGlobalIteration();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testVertexRemoval() {
+        super.testVertexRemoval();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testForceIndexUsage() {
+        super.testForceIndexUsage();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testSettingTTLOnNonStaticVertexLabel() throws Exception {
+        super.testSettingTTLOnNonStaticVertexLabel();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testTransactionConfiguration() {
+        super.testTransactionConfiguration();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testConsistencyEnforcement() {
+        super.testConsistencyEnforcement();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testHasNot() {
+        super.testHasNot();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testVertexTTLWithCompositeIndex() throws Exception {
+        super.testVertexTTLWithCompositeIndex();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testRelationTypeIndexes() {
+        super.testRelationTypeIndexes();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testGotGIndexRemoval() throws InterruptedException, ExecutionException {
+        super.testGotGIndexRemoval();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testTransactionIsolation() {
+        super.testTransactionIsolation();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testSelfLoop() {
+        super.testSelfLoop();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testIndexUniqueness() {
+        super.testIndexUniqueness();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testEdgeTTLWithTransactions() throws Exception {
+        super.testEdgeTTLWithTransactions();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testIndexQueryWithLabelsAndContainsIN() {
+        super.testIndexQueryWithLabelsAndContainsIN();
+    }
+
+    public interface IsolateTestEdgesExceedCacheSize {
+    }
+
+    @Test
+    @Override
+    @Category({IsolateTestEdgesExceedCacheSize.class})
+    public void testEdgesExceedCacheSize() {
+        super.testEdgesExceedCacheSize();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testThreadBoundTx() {
+        super.testThreadBoundTx();
+    }
+
+    @Test
+    @Override
+    @Category({MultiDynamoDBGraphTestCategory.class})
+    public void testCreateDelete() {
+        super.testCreateDelete();
     }
 }
