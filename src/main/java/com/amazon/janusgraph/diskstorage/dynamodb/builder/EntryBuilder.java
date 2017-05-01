88f86fab4bc2149273134c07f11ff2d9a6252b87
Alexander Patrikalakis <amcp@mit.edu>
2017-05-01 14:57:02 +0900

    Janusgraph support (#120)
    * Initial JanusGraph support.
    
    This is a large refactoring where I changed all the package names
    and prefixes to class names, and in general most occurrences of
    the word Titan. Subsequent commits on this branch will remove
    dependencies on legacy Titan code that was copied.
    
    * remove copied titan code and tests; fix testCompile target
    * Fix travis category package name, download GitHub zip, remove titan references
    * Fix log configuration. Follow redirects when downloading zip from GitHub
    * Updated versions in install script. Validated POM dependency tree
    * Server zip builds. Use remote sessions to maintain vars in Gremlin shell
    * Reduce log output by forcing use of log4j 1.2
    * Fix vertexCentricQuery and LogTest. Reorder initial test runs
    * Increase TIMEOUT_MS for certain Multi LogTests to 120s
    * Move IsolateGraphFailingTestCategory to JanusGraph package
    * Ignore MultiLogTest failures (created issues to track them)
    * fix testEdgesExceedCacheSize with tx-cache-size=16400

---
+++ src/main/java/com/amazon/janusgraph/diskstorage/dynamodb/builder/EntryBuilder.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.amazon.janusgraph.diskstorage.dynamodb.builder;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import com.amazon.janusgraph.diskstorage.dynamodb.Constants;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import org.janusgraph.diskstorage.Entry;
+import org.janusgraph.diskstorage.StaticBuffer;
+import org.janusgraph.diskstorage.util.BufferUtil;
+import org.janusgraph.diskstorage.util.StaticArrayEntry;
+
+/**
+ * EntryBuilder is responsible for translating from DynamoDB item maps to Entry
+ * objects.
+ *
+ * @author Matthew Sowders
+ * @author Alexander Patrikalakis
+ *
+ */
+public class EntryBuilder extends AbstractBuilder {
+    private final Map<String, AttributeValue> item;
+    private StaticBuffer start;
+    private StaticBuffer end;
+    private boolean slice;
+    private int limit = Integer.MAX_VALUE;
+
+    public EntryBuilder(Map<String, AttributeValue> item) {
+        this.item = item;
+        item.remove(Constants.TITAN_HASH_KEY);
+    }
+
+    public List<Entry> buildAll() {
+        if (null == item) {
+            return Collections.emptyList();
+        }
+        List<Entry> filteredEntries = new ArrayList<>(item.size());
+        final Entry sliceStartEntry = slice ? StaticArrayEntry.of(start, BufferUtil.emptyBuffer()) : null;
+        final Entry sliceEndEntry = slice ? StaticArrayEntry.of(end, BufferUtil.emptyBuffer()) : null;
+        for (String column : item.keySet()) {
+            StaticBuffer columnKey = decodeKey(column);
+            AttributeValue valueValue = item.get(column);
+            StaticBuffer value = decodeValue(valueValue);
+            final Entry entry = StaticArrayEntry.of(columnKey, value);
+            if(!slice || (entry.compareTo(sliceStartEntry) >= 0 && entry.compareTo(sliceEndEntry) < 0)) {
+                filteredEntries.add(StaticArrayEntry.of(columnKey, value));
+            }
+        }
+
+        //TODO(alexp) Arrays.parallelSort(filteredEntries) in JDK 8? Can you switch to java 8?
+        Collections.sort(filteredEntries);
+        return filteredEntries.subList(0, Math.min(filteredEntries.size(), limit));
+    }
+
+    public Entry build() {
+        if (null == item) {
+            return null;
+        }
+
+        StaticBuffer rangeKey = decodeKey(item, Constants.TITAN_RANGE_KEY);
+        return build(rangeKey);
+    }
+
+    public Entry build(StaticBuffer column) {
+        if (null == item || null == column) {
+            return null;
+        }
+
+        AttributeValue valueValue = item.get(Constants.TITAN_VALUE);
+        StaticBuffer value = decodeValue(valueValue);
+
+        // DynamoDB's between semantics include the end of a slice, but Titan expects the end to be exclusive
+        if(slice && column.compareTo(end) == 0) {
+            return null;
+        }
+
+        return StaticArrayEntry.of(column, value);
+    }
+
+    public EntryBuilder slice(StaticBuffer start, StaticBuffer end) {
+        this.start = start;
+        this.end = end;
+        this.slice = true;
+        return this;
+    }
+
+    public EntryBuilder limit(int limit) {
+        this.limit = limit;
+        return this;
+    }
+
+}
---
+++ src/main/java/com/amazon/janusgraph/diskstorage/dynamodb/iterator/MultiRowSequentialScanInterpreter.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.amazon.janusgraph.diskstorage.dynamodb.iterator;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import com.amazon.janusgraph.diskstorage.dynamodb.Constants;
+import com.amazon.janusgraph.diskstorage.dynamodb.DynamoDBStore;
+import com.amazon.janusgraph.diskstorage.dynamodb.QueryWorker;
+import com.amazon.janusgraph.diskstorage.dynamodb.builder.EntryBuilder;
+import com.amazon.janusgraph.diskstorage.dynamodb.builder.KeyBuilder;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.google.common.base.Optional;
+import com.google.common.collect.Lists;
+import org.janusgraph.diskstorage.Entry;
+import org.janusgraph.diskstorage.StaticBuffer;
+import org.janusgraph.diskstorage.keycolumnvalue.SliceQuery;
+import org.janusgraph.diskstorage.util.RecordIterator;
+
+/**
+ * Interprets Scan results for MULTI stores and assumes that results are SEQUENTIAL. This means that the scan is assumed to be non-segmented.
+ * We need this assumption because it makes it so we don't need to keep track of where segment boundaries lie in order to avoid returning duplicate
+ * hash keys.
+ */
+public class MultiRowSequentialScanInterpreter implements ScanContextInterpreter {
+
+    private final DynamoDBStore store;
+    private final SliceQuery sliceQuery;
+
+    public MultiRowSequentialScanInterpreter(DynamoDBStore store, SliceQuery sliceQuery) {
+        this.store = store;
+        this.sliceQuery = sliceQuery;
+    }
+
+    @Override
+    public List<SingleKeyRecordIterator> buildRecordIterators(ScanContext scanContext) {
+        final Map<String, AttributeValue> previousScanEnd = scanContext.getScanRequest().getExclusiveStartKey();
+
+        // If there was a previous request, we can assume we already returned a RecordIterator for the last hash key in the previous request
+        StaticBuffer previousKey = null;
+        if (previousScanEnd != null && !previousScanEnd.isEmpty()) {
+            previousKey = new KeyBuilder(previousScanEnd).build(Constants.TITAN_HASH_KEY);
+        }
+
+        final List<Map<String, AttributeValue>> items = scanContext.getScanResult().getItems();
+        final List<SingleKeyRecordIterator> recordIterators = Lists.newLinkedList();
+
+        final Iterator<Map<String, AttributeValue>> itemIterator = items.iterator();
+        while (itemIterator.hasNext()) {
+            final Optional<Map<String, AttributeValue>> nextItem = findItemWithDifferentHashKey(itemIterator, previousKey);
+            if (nextItem.isPresent()) {
+                final Map<String, AttributeValue> item = nextItem.get();
+                final StaticBuffer hashKey = new KeyBuilder(item).build(Constants.TITAN_HASH_KEY);
+
+                final Entry columnValue = new EntryBuilder(item).slice(sliceQuery.getSliceStart(),
+                                                                       sliceQuery.getSliceEnd())
+                                                                .build();
+                // If the range key value is equal to the upper bound of the slice query, columnValue will be null
+                // in these cases, we should not include the key in our list of iterators, because DynamoDB's upper bound
+                // is inclusive, but Titan's upper bound is exclusive
+                if (columnValue != null) {
+                    recordIterators.add(new SingleKeyRecordIterator(hashKey, buildRecordIteratorForHashKey(hashKey)));
+                }
+                // It's always okay to move on to the next key after finding a single result, because the columns for each hash key are
+                // scanned in order.
+                previousKey = hashKey;
+            }
+        }
+
+        return recordIterators;
+    }
+
+    private Optional<Map<String, AttributeValue>> findItemWithDifferentHashKey(Iterator<Map<String, AttributeValue>> itemIterator, StaticBuffer previousKey) {
+        Optional<Map<String, AttributeValue>> result = Optional.absent();
+
+        while (itemIterator.hasNext() && !result.isPresent()) {
+            final Map<String, AttributeValue> item = itemIterator.next();
+            final StaticBuffer nextKey = new KeyBuilder(item).build(Constants.TITAN_HASH_KEY);
+            if (!nextKey.equals(previousKey)) {
+                result = Optional.of(item);
+            }
+        }
+        return result;
+    }
+
+    private RecordIterator<Entry> buildRecordIteratorForHashKey(StaticBuffer hashKey) {
+        final QueryWorker queryWorker = store.buildQueryWorker(hashKey, sliceQuery);
+        return new MultiRecordIterator(queryWorker, sliceQuery);
+    }
+}
