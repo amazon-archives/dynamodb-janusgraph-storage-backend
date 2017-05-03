b05e88a37e3a44d4e0697cc557be269e3396bad8
Alexander Patrikalakis <amcp@amazon.co.jp>
2017-05-03 23:58:05 +0900

    Use sdkman to speed things up

--- src/test/resources/install-gremlin-server.sh
+++ src/test/resources/install-gremlin-server.sh
@@ -17,7 +17,8 @@ set -eux
 #
 
 #collect the prereqs and build the plugin
-mvn clean install verify -DskipTests=true
+export MAVEN_OPTS="-XX:+TieredCompilation -XX:TieredStopAtLevel=1"
+mvn -T 1C install -Dmaven.test.skip=true -DskipTests=true
 
 # Directory structure of server directory
 # -src
